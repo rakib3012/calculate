fcd83886285cf92aba5f1e2fbfc1ebb5737b8c4f
Rakib <rakibuzzaman1177@gmail.com>
2023-05-31 22:35:40 +0600

    js add

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+ 
+ 
+
+
+const Tbtn =  document.getElementById("Tbtn");
+
+ 
+ 
+ Tbtn.addEventListener('click',function hi (){
+    const a = document.getElementById("Ta").value;
+    const b = document.getElementById("Tb").value;
+    area = a + b;
+   const triangle = document.getElementById("ts2");
+   triangle.innerHTML ="triangle2 is :" + '' + area;
+     
+}) ;
+
+
+
+
+
+ 
+
+ 
+
+
+
+  
